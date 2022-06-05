cbb49d23bed81922eff7f9eb417adeecbc2225fe
Kristina Tuzova <ktuzova1901@gmail.com>
2022-06-05 05:14:34 +0300

    arrow

---
+++ js/arrow.js
@@ -0,0 +1,10 @@
+// let arrow = document.querySelectorAll(".arrow");
+// for (i = 0; i < arrow.length; i++) {
+//   let thisLink = arrow.prviousSibling;
+//   let submenu = arrow[i].nextSibling;
+//   let thisArrow = arrow[i];
+//   arrow[i].addEventListener("click", function () {
+//     submenu.classList.toggle("open");
+//     thisArrow.classList.toggle("active");
+//   });
+// }
